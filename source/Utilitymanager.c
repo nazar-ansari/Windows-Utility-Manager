26bcd142818d3449919427286c1539a0d89dbe2d
Nazar Ansari <nazaransari06@gmail.com>
2022-04-24 22:40:01 +0530

    Defined Custom Pattern

--- source/Utilitymanager.c
+++ source/Utilitymanager.c
@@ -2,3 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+/*                                              Defined Custom Pattern For In-Between Program                             */
+#define PATTERN1 1
+#define PATTERN2 2
+#define PATTERN3 3

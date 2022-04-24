fe335dd195f4dbf46b33da764e8670bdb4b5db3c
Nazar Ansari <nazaransari06@gmail.com>
2022-04-24 22:41:35 +0530

    Declared System Prototype Function

--- source/Utilitymanager.c
+++ source/Utilitymanager.c
@@ -6,3 +6,11 @@
 #define PATTERN1 1
 #define PATTERN2 2
 #define PATTERN3 3
+/*                                                  Initiated Pattern Prototype Function                                            */
+int pattern(int variable, int limit);
+/*                                                     Declared  System Prototype                                                   */
+int sysmain()
+{
+    system("cls");
+    main();
+}
